a801cd470f1b7106d573c417087a512e64a6553f
Kaitian Xie <xkaitian@gmail.com>
2019-04-03 15:12:00 -0700

    200: Add solution 1

--- src/com/algobot76/_200/Solution1.java
+++ src/com/algobot76/_200/Solution1.java
@@ -1,7 +1,41 @@
 package com.algobot76._200;
 
+/**
+ * This solution is solved by using DFS.
+ */
 class Solution1 {
     public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
 
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int numIslands = 0;
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                if (grid[r][c] == '1') {
+                    ++numIslands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    void dfs(char[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
     }
 }
