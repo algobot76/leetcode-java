a5f15e44ddc64e206c7f6ab7fca7d43f0592c854
Kaitian Xie <xkaitian@gmail.com>
2019-04-03 18:21:09 -0700

    141: Add solution 2

---
+++ src/com/algobot76/_141/Solution2.java
@@ -0,0 +1,25 @@
+package com.algobot76._141;
+
+import com.algobot76.util.ListNode;
+
+/**
+ * This problem is solved by using two pointers.
+ */
+public class Solution2 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
