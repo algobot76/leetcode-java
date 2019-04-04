e7872008dfbc3180b9c4bd87583cff784dfb1304
Kaitian Xie <xkaitian@gmail.com>
2019-04-03 18:16:33 -0700

    141: Finish solution 1

--- src/com/algobot76/_141/Solution1.java
+++ src/com/algobot76/_141/Solution1.java
@@ -1,18 +1,24 @@
 package com.algobot76._141;
 
+import com.algobot76.util.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * This problem is solved by using a hash table.
  */
 public class Solution1 {
     public boolean hasCycle(ListNode head) {
-
+        Set<ListNode> visited = new HashSet<>();
+        while (head != null){
+            if(visited.contains(head)){
+                return true;
+            } else {
+                visited.add(head);
+            }
+            head = head.next;
+        }
+        return false;
     }
 }
