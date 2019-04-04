de122107d2ab91e8cbd8d5c1e7d1ee64622c94cc
Kaitian Xie <xkaitian@gmail.com>
2019-04-03 18:06:24 -0700

    141: Begin problem

---
+++ src/com/algobot76/_141/Solution1.java
@@ -0,0 +1,18 @@
+package com.algobot76._141;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution1 {
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
