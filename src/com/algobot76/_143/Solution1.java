6540154160c1fc755e3d42569841abdc237ce08b
Kaitian Xie <xkaitian@gmail.com>
2019-04-04 15:19:29 -0700

    143: Finish problem

--- src/com/algobot76/_143/Solution1.java
+++ src/com/algobot76/_143/Solution1.java
@@ -4,6 +4,41 @@ import com.algobot76.util.ListNode;
 
 public class Solution1 {
     public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode mid = findMid(head);
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverse(l2);
+        ListNode l1 = head;
+        while (l1 != null && l2 != null) {
+            ListNode next = l1.next;
+            l1.next = l2;
+            l2 = l2.next;
+            l1.next.next = next;
+            l1 = next;
+        }
+    }
 
+    private ListNode findMid(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
     }
 }
