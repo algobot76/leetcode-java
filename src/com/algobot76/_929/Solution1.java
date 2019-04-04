2b62d2115f773042a4389ac2d770d0f38c40ab5e
Kaitian Xie <xkaitian@gmail.com>
2019-04-04 15:47:39 -0700

    929: Add solution 1

--- src/com/algobot76/_929/Solution1.java
+++ src/com/algobot76/_929/Solution1.java
@@ -1,7 +1,16 @@
 package com.algobot76._929;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution1 {
     public int numUniqueEmails(String[] emails) {
-
+        Set<String> visited = new HashSet<>();
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            String[] local = parts[0].split("\\+");
+            visited.add(local[0].replace(".", "") + "@" + parts[1]);
+        }
+        return visited.size();
     }
 }
