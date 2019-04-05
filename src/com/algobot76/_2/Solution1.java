ffce19f0b32a5e2d355b1542041be1e6e7eeaea1
Kaitian Xie <xkaitian@gmail.com>
2019-04-05 13:22:50 -0700

    2: Add solution 1

--- src/com/algobot76/_2/Solution1.java
+++ src/com/algobot76/_2/Solution1.java
@@ -4,6 +4,28 @@ import com.algobot76.util.ListNode;
 
 public class Solution1 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 }
