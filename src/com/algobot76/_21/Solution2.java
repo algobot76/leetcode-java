2dbc199d492728bcd909b6dbe61704b80f8c773a
Kaitian Xie <xkaitian@gmail.com>
2019-04-06 17:21:15 -0700

    21: Add solution 2

---
+++ src/com/algobot76/_21/Solution2.java
@@ -0,0 +1,29 @@
+package com.algobot76._21;
+
+import com.algobot76.util.ListNode;
+
+/**
+ * Iteration
+ */
+public class Solution2 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 == null) {
+            curr.next = l2;
+        } else {
+            curr.next = l1;
+        }
+        return dummyHead.next;
+    }
+}
