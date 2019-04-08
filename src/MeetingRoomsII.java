cfaf790e6354299f04fb1fbd44413ac669d7c87d
Kaitian Xie <xkaitian@gmail.com>
2019-04-07 22:42:12 -0700

    253: Priority Queue

--- src/MeetingRoomsII.java
+++ src/MeetingRoomsII.java
@@ -1,5 +1,9 @@
 import util.Interval;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 253. Meeting Rooms II
  *
@@ -7,6 +11,20 @@ import util.Interval;
  */
 public class MeetingRoomsII {
     public int minMeetingRooms(Interval[] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
 
+        PriorityQueue<Integer> allocator = new PriorityQueue<>(intervals.length, Comparator.comparingInt(a -> a));
+        Arrays.sort(intervals, Comparator.comparingInt(l -> l.start));
+        allocator.add(intervals[0].end);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= allocator.peek()) {
+                allocator.poll();
+            }
+            allocator.add(intervals[i].end);
+        }
+
+        return allocator.size();
     }
 }
