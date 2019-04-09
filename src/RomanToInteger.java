2bbc3c4b2960fdc59a48f610826c02f632d6d9b8
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 13:10:59 -0700

    13: Hash Table

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -1,8 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  */
 public class RomanToInteger {
-    public int romanToInt(String s) {
+    public static int romanToInt(String s) {
+        if (s == null || s.isEmpty()) {
+            return -1;
+        }
 
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int len = s.length();
+        int result = map.get(s.charAt(len - 1));
+        for (int i = len - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1)))
+                result += map.get(s.charAt(i));
+            else
+                result -= map.get(s.charAt(i));
+        }
+
+        return result;
     }
 }
