84589b3a14204ae654ec0e3d8eceb8aeadb018ee
Kaitian Xie <xkaitian@gmail.com>
2019-04-08 22:35:30 -0700

    20: Stack

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,5 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 20. Valid Parentheses
+ *
+ * @author Kaitian Xie
+ */
 public class ValidParentheses {
     public boolean isValid(String s) {
+        Map<Character, Character> mapppings = new HashMap<>();
+        mapppings.put(')', '(');
+        mapppings.put('}', '{');
+        mapppings.put(']', '[');
+        Deque<Character> stack = new ArrayDeque<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (mapppings.containsKey(ch)) {
+                char top = stack.isEmpty() ? '#' : stack.pop();
+                if (top != mapppings.get(ch)) {
+                    return false;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
