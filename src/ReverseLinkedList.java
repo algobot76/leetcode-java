c6cc4350245ba8355b1b332531b5f1f231ec2b7f
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 05:13:50 -0700

    503: Recurrsion

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,7 +1,20 @@
 import util.ListNode;
 
+/**
+ * 206. Reverse Linked List
+ *
+ * @author Kaitian Xie
+ */
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
 
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return p;
     }
 }
