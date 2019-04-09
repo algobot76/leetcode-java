2da32d4009db8fbfdd3d4ce545093de072fcbb28
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 04:56:34 -0700

    53: Dynamic Programming

--- src/MaximumSubarray.java
+++ src/MaximumSubarray.java
@@ -1,5 +1,16 @@
-public class MaximumSubarray {
-    public int maxSubArray(int[] nums) {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class MaximumSubarray {
+    // Dynamic Programming
+    public int maxSubArray(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+
+        return Collections.max(Arrays.asList(dp));
     }
 }
