b1db5df271015e5097a6668d9d709a2859218ba6
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 20:06:28 -0700

    155: Two Stacks

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,26 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 155. Min Stack
  *
  * @author Kaitian Xie
  */
 public class MinStack {
-    public MinStack() {
+    private Deque<Integer> stack;
+    private Deque<Integer> min;
 
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.min = new ArrayDeque<>();
     }
 
     public void push(int x) {
-
+        if (min.isEmpty()) {
+            min.push(x);
+        } else if (x <= getMin()) {
+            min.push(x);
+        }
+        stack.push(x);
     }
 
     public void pop() {
-
+        if (stack.pop() == getMin()) {
+            min.pop();
+        }
     }
 
     public int top() {
-
+        return stack.peek();
     }
 
     public int getMin() {
-
+        return min.peek();
     }
 }
