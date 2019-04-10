2769e5bd2bc2a782c7554b370fb0fec242142d96
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 21:00:48 -0700

    215: Heap

--- src/KthLargestElementInAnArray.java
+++ src/KthLargestElementInAnArray.java
@@ -1,9 +1,25 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class KthLargestElementInAnArray {
     // Sort
+//    public int findKthLargest(int[] nums, int k) {
+//        Arrays.sort(nums);
+//        return nums[nums.length-k];
+//    }
+
+    // Heap
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        Queue<Integer> heap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        return heap.poll();
     }
 }
