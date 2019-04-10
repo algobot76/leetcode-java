1c3b628a7e77d568cdfb5b8c70a664ed7549a676
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 15:20:06 -0700

    344: Two Pointers

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -5,6 +5,15 @@
  */
 public class ReverseString {
     public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
 
+        while (start < end) {
+            char temp = s[end];
+            s[end] = s[start];
+            s[start] = temp;
+            start++;
+            end--;
+        }
     }
 }
