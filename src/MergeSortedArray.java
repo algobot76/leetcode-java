0c0913273c0bfa33e0c30591b2a90325f4733144
Kaitian Xie <xkaitian@gmail.com>
2019-04-09 18:51:55 -0700

    88: Naive Approach

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 /**
  * 88. Merge Sorted Array
  *
  * @author Kaitian Xie
  */
 public class MergeSortedArray {
+    // Naive Approach
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
     }
 }
