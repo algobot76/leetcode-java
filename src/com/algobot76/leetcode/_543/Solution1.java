4c6fc60a8297fdde0107497753639af9e8c569f3
Kaitian Xie <xkaitian@gmail.com>
2019-04-11 15:02:33 -0700

    543: Finish problem

--- src/com/algobot76/leetcode/_543/Solution1.java
+++ src/com/algobot76/leetcode/_543/Solution1.java
@@ -2,8 +2,24 @@ package com.algobot76.leetcode._543;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+/**
+ * DFS
+ */
 public class Solution1 {
-    public int diameterOfBinaryTree(TreeNode root) {
+    int ans = 0;
 
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return ans;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        ans = Math.max(ans, left + right);
+        return 1 + Math.max(left, right);
     }
 }
