1edd24a0880e6dc1a8db27921d3f5ed3af710c1e
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 20:45:06 -0700

    Finish reorge of problem 13

--- src/com/algobot76/leetcode/_13/Solution2.java
+++ src/com/algobot76/leetcode/_13/Solution2.java
@@ -1,37 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
+package com.algobot76.leetcode._13;
 
-/**
- *
- */
-public class RomanToInteger {
-    // Hash Table
-//    public static int romanToInt(String s) {
-//        if (s == null || s.isEmpty()) {
-//            return -1;
-//        }
-//
-//        Map<Character, Integer> map = new HashMap<Character, Integer>();
-//        map.put('I', 1);
-//        map.put('V', 5);
-//        map.put('X', 10);
-//        map.put('L', 50);
-//        map.put('C', 100);
-//        map.put('D', 500);
-//        map.put('M', 1000);
-//
-//        int len = s.length();
-//        int result = map.get(s.charAt(len - 1));
-//        for (int i = len - 2; i >= 0; i--) {
-//            if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1)))
-//                result += map.get(s.charAt(i));
-//            else
-//                result -= map.get(s.charAt(i));
-//        }
-//
-//        return result;
-//    }
-
+public class Solution2 {
     public int romanToInt(String s) {
         int index = 0;
         int ans = 0;
