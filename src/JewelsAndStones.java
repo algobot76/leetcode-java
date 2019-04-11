52ac9eac81ccae18e83252b11e6664dfe588a10d
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 17:02:58 -0700

    771: Hash Table

--- src/JewelsAndStones.java
+++ src/JewelsAndStones.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 771. Jewels and Stones
  *
@@ -5,6 +8,17 @@
  */
 public class JewelsAndStones {
     public int numJewelsInStones(String J, String S) {
+        Set<Character> jSet = new HashSet<>();
+        for (char j : J.toCharArray()) {
+            jSet.add(j);
+        }
 
+        int count = 0;
+        for (char s : S.toCharArray()) {
+            if (jSet.contains(s)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
