0da89c210344c921cfb83535f9036859b4f58f26
Kaitian Xie <xkaitian@gmail.com>
2019-04-11 11:26:36 -0700

    Implement TreeNode

---
+++ src/com/algobot76/leetcode/util/TreeNode.java
@@ -0,0 +1,11 @@
+package com.algobot76.leetcode.util;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
