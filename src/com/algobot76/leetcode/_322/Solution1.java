0c6681d370d285851e05fdb9e3da418cd5f6c71f
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 20:41:20 -0700

    Finish reorge of problem 322

---
+++ src/com/algobot76/leetcode/_322/Solution1.java
@@ -0,0 +1,37 @@
+package com.algobot76.leetcode._322;
+
+/**
+ * Dynamic Programming (Top Down)
+ */
+public class Solution1 {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int rem, int[] dp) {
+        if (rem < 0) {
+            return -1;
+        }
+        if (rem == 0) {
+            return 0;
+        }
+        if (dp[rem - 1] != 0) {
+            return dp[rem - 1];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, dp);
+            if (res >= 0 && res < min) {
+                min = 1 + res;
+            }
+        }
+        dp[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+
+        return dp[rem - 1];
+    }
+}
