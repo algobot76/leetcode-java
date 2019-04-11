b78ed844011037e32b57671caf09ca21c365b904
Kaitian Xie <xkaitian@gmail.com>
2019-04-11 11:22:53 -0700

    412: Finish problem

--- src/com/algobot76/leetcode/_412/Solution1.java
+++ src/com/algobot76/leetcode/_412/Solution1.java
@@ -1,9 +1,24 @@
 package com.algobot76.leetcode._412;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution1 {
     public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
 
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.add("Fizz");
+            } else if (i % 5 == 0) {
+                ans.add("Buzz");
+            } else {
+                ans.add(Integer.toString(i));
+            }
+        }
+
+        return ans;
     }
 }
