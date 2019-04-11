45f637987665ae29e8051d37d8b82f5cc6ad833f
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 20:34:36 -0700

    Finish reorg of TwoSum

---
+++ src/com/algobot76/leetcode/_1/Solution2.java
@@ -0,0 +1,23 @@
+package com.algobot76.leetcode._1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Two-Pass Hash Table
+ */
+public class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
---
+++ src/com/algobot76/leetcode/_1/Solution3.java
@@ -0,0 +1,22 @@
+package com.algobot76.leetcode._1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * One-Pass Hash Table
+ */
+public class Solution3 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+}
