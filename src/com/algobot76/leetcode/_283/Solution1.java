1e6fd5da8e094bbc55365b671ef7357d46d56a23
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 23:04:51 -0700

    283: Finish problem

--- src/com/algobot76/leetcode/_283/Solution1.java
+++ src/com/algobot76/leetcode/_283/Solution1.java
@@ -2,6 +2,22 @@ package com.algobot76.leetcode._283;
 
 public class Solution1 {
     public void moveZeroes(int[] nums) {
+        int count = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+                continue;
+            }
+            if (count > 0) {
+                swap(nums, i, i - count);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
