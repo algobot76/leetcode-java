140e9584cdf8400388c4a76083e4de51d34ac556
Kaitian Xie <xkaitian@gmail.com>
2019-04-10 22:17:49 -0700

    Finish reorg for problem 200

--- src/com/algobot76/leetcode/_200/Solution2.java
+++ src/com/algobot76/leetcode/_200/Solution2.java
@@ -1,49 +1,12 @@
+package com.algobot76.leetcode._200;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 200. Number of Islands
- *
- * @author Kaitian Xie
+ * BFS
  */
-class NumberOfIslands {
-    // DFS
-//    public int numIslands(char[][] grid) {
-//        if (grid == null || grid.length == 0) {
-//            return 0;
-//        }
-//
-//        int rows = grid.length;
-//        int cols = grid[0].length;
-//        int numIslands = 0;
-//        for (int r = 0; r < rows; ++r) {
-//            for (int c = 0; c < cols; ++c) {
-//                if (grid[r][c] == '1') {
-//                    ++numIslands;
-//                    dfs(grid, r, c);
-//                }
-//            }
-//        }
-//
-//        return numIslands;
-//    }
-
-//    private void dfs(char[][] grid, int r, int c) {
-//        int rows = grid.length;
-//        int cols = grid[0].length;
-//
-//        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
-//            return;
-//        }
-//
-//        grid[r][c] = '0';
-//        dfs(grid, r - 1, c);
-//        dfs(grid, r + 1, c);
-//        dfs(grid, r, c - 1);
-//        dfs(grid, r, c + 1);
-//    }
-
-    // BFS
+public class Solution2 {
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
