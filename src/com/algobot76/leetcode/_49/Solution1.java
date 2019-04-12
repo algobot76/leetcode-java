6a0b0a39bbecc139b1cd9ad1845de3390d3e5572
Kaitian Xie <xkaitian@gmail.com>
2019-04-12 03:58:14 -0700

    49: Categorize by Sorted String

--- src/com/algobot76/leetcode/_49/Solution1.java
+++ src/com/algobot76/leetcode/_49/Solution1.java
@@ -1,9 +1,24 @@
 package com.algobot76.leetcode._49;
 
-import java.util.List;
+import java.util.*;
 
+/**
+ * Categorize by Sorted String
+ */
 public class Solution1 {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List> ans = new HashMap<>();
 
+        for (String str : strs) {
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String key = new String(ca);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(str);
+        }
+
+        return new ArrayList(ans.values());
     }
 }
