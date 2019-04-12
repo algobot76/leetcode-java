02281002250dc4c2f2eda88917ffa07e58cbe69e
Kaitian Xie <xkaitian@gmail.com>
2019-04-11 22:06:21 -0700

    238: Brute Force

--- src/com/algobot76/leetcode/_238/Solution1.java
+++ src/com/algobot76/leetcode/_238/Solution1.java
@@ -1,7 +1,29 @@
 package com.algobot76.leetcode._238;
 
+/**
+ * Brute Force (Time Limit Exceeded)
+ */
 public class Solution1 {
     public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
 
+        for (int i = 0; i < len; i++) {
+            ans[i] = prod(i, nums);
+        }
+
+        return ans;
+    }
+
+    private int prod(int i, int[] nums) {
+        int result = 1;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (j != i) {
+                result *= nums[j];
+            }
+        }
+
+        return result;
     }
 }
