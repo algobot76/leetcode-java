aba9a77cdcfcc3cd8540a659a4fae14bb311018e
Kaitian Xie <xkaitian@gmail.com>
2019-04-12 19:26:02 -0700

    189: Use an extra array

--- src/com/algobot76/leetcode/_189/Solution1.java
+++ src/com/algobot76/leetcode/_189/Solution1.java
@@ -2,6 +2,14 @@ package com.algobot76.leetcode._189;
 
 public class Solution1 {
     public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int[] copy = new int[len];
 
+        for (int i = 0; i < len; i++) {
+            copy[(i + k) % len] = nums[i];
+        }
+        for (int i = 0; i < len; i++) {
+            nums[i] = copy[i];
+        }
     }
 }
