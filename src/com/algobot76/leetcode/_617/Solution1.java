e660a5056ebb1496f32cb552547a124989ff5de2
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 12:00:27 -0700

    617: Recursion

--- src/com/algobot76/leetcode/_617/Solution1.java
+++ src/com/algobot76/leetcode/_617/Solution1.java
@@ -2,8 +2,21 @@ package com.algobot76.leetcode._617;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+/**
+ * Recursion
+ */
 public class Solution1 {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
 
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
     }
 }
