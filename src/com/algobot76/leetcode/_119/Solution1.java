f917ba4ec8cd89434cda3236202d404c19908b46
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 11:07:13 -0700

    119: Finish problem

--- src/com/algobot76/leetcode/_119/Solution1.java
+++ src/com/algobot76/leetcode/_119/Solution1.java
@@ -1,9 +1,20 @@
 package com.algobot76.leetcode._119;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution1 {
-    public List<Integer> getRow(int rowIndex) {
+    public List<Integer> getRow(int k) {
+        Integer[] ans = new Integer[k + 1];
+        Arrays.fill(ans, 0);
+        ans[0] = 1;
 
+        for (int i = 1; i <= k; i++) {
+            for (int j = i; j > 0; j--) {
+                ans[j] = ans[j] + ans[j - 1];
+            }
+        }
+
+        return Arrays.asList(ans);
     }
 }
