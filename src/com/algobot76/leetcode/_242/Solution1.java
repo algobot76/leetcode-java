45daf1d41b591bc49cd4f74ae204bc492a41d3ea
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 16:40:16 -0700

    242: Sorting

--- src/com/algobot76/leetcode/_242/Solution1.java
+++ src/com/algobot76/leetcode/_242/Solution1.java
@@ -1,7 +1,21 @@
 package com.algobot76.leetcode._242;
 
+import java.util.Arrays;
+
+/**
+ * Sorting
+ */
 public class Solution1 {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
 
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+
+        return Arrays.equals(sa, ta);
     }
 }
