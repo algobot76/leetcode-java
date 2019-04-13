bf757b20c8eb96aeff41dfc9985dd4b4db8714ab
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 12:21:54 -0700

    127: Begin problem

---
+++ src/com/algobot76/leetcode/_127/Solution1.java
@@ -0,0 +1,9 @@
+package com.algobot76.leetcode._127;
+
+import java.util.List;
+
+public class Solution1 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+    }
+}
