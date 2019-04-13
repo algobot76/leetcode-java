f8c63d1f9e50487a0625aab2edd22c38237e72dc
Kaitian Xie <xkaitian@gmail.com>
2019-04-12 21:08:06 -0700

    73: Use additional memory

--- src/com/algobot76/leetcode/_73/Solution1.java
+++ src/com/algobot76/leetcode/_73/Solution1.java
@@ -1,7 +1,30 @@
 package com.algobot76.leetcode._73;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution1 {
     public void setZeroes(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
 
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
