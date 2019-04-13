e7e7b68903ee44d1d6350db995b041f74b313647
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 08:26:56 -0700

    48: Transpose + reverse

--- src/com/algobot76/leetcode/_48/Solution1.java
+++ src/com/algobot76/leetcode/_48/Solution1.java
@@ -1,7 +1,35 @@
 package com.algobot76.leetcode._48;
 
+/**
+ * Transpose + Reverse
+ */
 public class Solution1 {
     public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
 
+    private void transpose(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    private void reverse(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
     }
 }
