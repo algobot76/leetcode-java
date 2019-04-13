5f8b871690a4a5b58d26635909e1de75f7468f81
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 14:55:03 -0700

    160: Two Pointers

--- src/com/algobot76/leetcode/_160/Solution1.java
+++ src/com/algobot76/leetcode/_160/Solution1.java
@@ -2,8 +2,46 @@ package com.algobot76.leetcode._160;
 
 import com.algobot76.leetcode.util.ListNode;
 
+/**
+ * Two Pointers
+ */
 public class Solution1 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
 
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+        if (ptrA == ptrB) {
+            return ptrA;
+        }
+        while (ptrA != null && ptrB != null) {
+            ptrA = ptrA.next;
+            ptrB = ptrB.next;
+        }
+        if (ptrA == null) {
+            ptrA = headB;
+        } else {
+            ptrB = headA;
+        }
+        while (ptrA != null && ptrB != null) {
+            ptrA = ptrA.next;
+            ptrB = ptrB.next;
+        }
+        if (ptrA == null) {
+            ptrA = headB;
+        } else {
+            ptrB = headA;
+        }
+        while (ptrA != null && ptrB != null) {
+            if (ptrA == ptrB) {
+                return ptrA;
+            }
+            ptrA = ptrA.next;
+            ptrB = ptrB.next;
+        }
+
+        return null;
     }
 }
