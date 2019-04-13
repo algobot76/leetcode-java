4ae55340830929ccf3c5f3ab3df761767658c57a
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 14:02:51 -0700

    160: Begin problem

---
+++ src/com/algobot76/leetcode/_160/Solution1.java
@@ -0,0 +1,9 @@
+package com.algobot76.leetcode._160;
+
+import com.algobot76.leetcode.util.ListNode;
+
+public class Solution1 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+}
