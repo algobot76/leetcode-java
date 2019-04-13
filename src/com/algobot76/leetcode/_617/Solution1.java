0b5fc3c36346ac285c3d9a8875efb4b9b7f48705
Kaitian Xie <xkaitian@gmail.com>
2019-04-13 11:56:48 -0700

    617: Begin problem

---
+++ src/com/algobot76/leetcode/_617/Solution1.java
@@ -0,0 +1,9 @@
+package com.algobot76.leetcode._617;
+
+import com.algobot76.leetcode.util.TreeNode;
+
+public class Solution1 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+    }
+}
