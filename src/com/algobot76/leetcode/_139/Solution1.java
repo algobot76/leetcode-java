32c698dd5cc8f3749aa9f9268317fbcf89e9a5b7
Kaitian Xie <xkaitian@gmail.com>
2019-04-14 15:50:47 -0700

    133: Recursion + memorization

--- src/com/algobot76/leetcode/_139/Solution1.java
+++ src/com/algobot76/leetcode/_139/Solution1.java
@@ -1,9 +1,32 @@
 package com.algobot76.leetcode._139;
 
-import java.util.List;
+import java.util.*;
 
+/**
+ * Recursion + Memorization
+ */
 public class Solution1 {
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        Map<String, Boolean> mem = new HashMap<>();
+        return wordBreakHelper(s, mem, dict);
+    }
 
+    private boolean wordBreakHelper(String s, Map<String, Boolean> mem, Set<String> dict) {
+        if (mem.containsKey(s)) {
+            return mem.get(s);
+        }
+        if (dict.contains(s)) {
+            mem.put(s, true);
+            return true;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            if (dict.contains(s.substring(i)) && wordBreakHelper(s.substring(0, i), mem, dict)) {
+                mem.put(s, true);
+                return true;
+            }
+        }
+        mem.put(s, false);
+        return false;
     }
 }
