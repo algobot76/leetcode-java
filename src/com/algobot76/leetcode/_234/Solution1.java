7fbd944d290be84a24c727dfb7fe266d7c1870c2
Kaitian Xie <xkaitian@gmail.com>
2019-04-14 20:02:21 -0700

    234: Finish problem

--- src/com/algobot76/leetcode/_234/Solution1.java
+++ src/com/algobot76/leetcode/_234/Solution1.java
@@ -4,6 +4,40 @@ import com.algobot76.leetcode.util.ListNode;
 
 public class Solution1 {
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
 
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode ptrA = head;
+        ListNode ptrB = reverse(slow);
+        while (ptrB != null) {
+            if (ptrA.val != ptrB.val) {
+                return false;
+            }
+            ptrA = ptrA.next;
+            ptrB = ptrB.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
