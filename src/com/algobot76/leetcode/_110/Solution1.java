6682c4effb8365c0df75b0fc421434cd88c0ce9d
Kaitian Xie <xkaitian@gmail.com>
2019-04-15 12:09:32 -0700

    110: Brute Force

--- src/com/algobot76/leetcode/_110/Solution1.java
+++ src/com/algobot76/leetcode/_110/Solution1.java
@@ -2,8 +2,23 @@ package com.algobot76.leetcode._110;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+/**
+ * Brute Force
+ */
 public class Solution1 {
     public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
 
+        return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
