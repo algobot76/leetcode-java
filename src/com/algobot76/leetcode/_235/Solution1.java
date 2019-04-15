c8c4e5f198c91198ccc501c3534265486d15f01e
Kaitian Xie <xkaitian@gmail.com>
2019-04-14 19:21:17 -0700

    235: Begin problem

---
+++ src/com/algobot76/leetcode/_235/Solution1.java
@@ -0,0 +1,9 @@
+package com.algobot76.leetcode._235;
+
+import com.algobot76.leetcode.util.TreeNode;
+
+public class Solution1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
