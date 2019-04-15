4a05c0662a457aff84fce1a8252cd683311cc2e7
Kaitian Xie <xkaitian@gmail.com>
2019-04-15 13:15:31 -0700

    108: Divide and Conquer

--- src/com/algobot76/leetcode/_108/Solution1.java
+++ src/com/algobot76/leetcode/_108/Solution1.java
@@ -2,8 +2,23 @@ package com.algobot76.leetcode._108;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+/**
+ * Divide and Conquer
+ */
 public class Solution1 {
     public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
 
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, start, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, end);
+        return node;
     }
 }
