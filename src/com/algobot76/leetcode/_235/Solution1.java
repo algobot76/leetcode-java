a40766833fbc960c7d406763b9326d178be000b3
Kaitian Xie <xkaitian@gmail.com>
2019-04-14 19:24:14 -0700

    235: Finish problem

--- src/com/algobot76/leetcode/_235/Solution1.java
+++ src/com/algobot76/leetcode/_235/Solution1.java
@@ -4,6 +4,12 @@ import com.algobot76.leetcode.util.TreeNode;
 
 public class Solution1 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
     }
 }
