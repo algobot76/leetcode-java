de9fedfcf7192f868f078393ff44ca0442250eb7
Kaitian Xie <xkaitian@gmail.com>
2019-04-15 11:09:50 -0700

    111: Depth-First Traversal

--- src/com/algobot76/leetcode/_111/Solution1.java
+++ src/com/algobot76/leetcode/_111/Solution1.java
@@ -2,8 +2,21 @@ package com.algobot76.leetcode._111;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+/**
+ * Depth-First Traversal
+ */
 public class Solution1 {
     public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
 
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
