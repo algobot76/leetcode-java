97a127f6f20b9849853cf40dceb6d24580b65e2d
Kaitian Xie <xkaitian@gmail.com>
2019-04-14 20:30:07 -0700

    387: Finish problem

--- src/com/algobot76/leetcode/_387/Solution1.java
+++ src/com/algobot76/leetcode/_387/Solution1.java
@@ -2,6 +2,17 @@ package com.algobot76.leetcode._387;
 
 public class Solution1 {
     public int firstUniqChar(String s) {
+        int[] count = new int[26];
 
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        for (char ch : s.toCharArray()) {
+            if (count[ch - 'a'] == 1) {
+                return s.indexOf(ch);
+            }
+        }
+
+        return -1;
     }
 }
