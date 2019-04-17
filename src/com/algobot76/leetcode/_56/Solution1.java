7e238f9b548ed6b758d8da9ad96dfd52e70f0d54
Kaitian Xie <xkaitian@gmail.com>
2019-04-17 10:58:54 -0700

    56: Begin problem

---
+++ src/com/algobot76/leetcode/_56/Solution1.java
@@ -0,0 +1,11 @@
+package com.algobot76.leetcode._56;
+
+import com.algobot76.leetcode.util.Interval;
+
+import java.util.List;
+
+public class Solution1 {
+    public List<Interval> merge(List<Interval> intervals) {
+
+    }
+}
