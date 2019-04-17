3673ae8b511d22716660843b8806798cbe534526
Kaitian Xie <xkaitian@gmail.com>
2019-04-17 11:41:19 -0700

    9: Finish prooblem

--- src/com/algobot76/leetcode/_9/Solution1.java
+++ src/com/algobot76/leetcode/_9/Solution1.java
@@ -2,6 +2,16 @@ package com.algobot76.leetcode._9;
 
 public class Solution1 {
     public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
 
+        int num = 0;
+        while (x > num) {
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == num || x == num / 10;
     }
 }
