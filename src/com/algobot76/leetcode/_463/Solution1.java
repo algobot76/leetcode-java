1135b88a49195ed4187cbea6d5a08650e443a3e3
Kaitian Xie <xkaitian@gmail.com>
2019-04-18 11:10:21 -0700

    463: Finish problem

--- src/com/algobot76/leetcode/_463/Solution1.java
+++ src/com/algobot76/leetcode/_463/Solution1.java
@@ -2,6 +2,48 @@ package com.algobot76.leetcode._463;
 
 public class Solution1 {
     public int islandPerimeter(int[][] grid) {
+        int sum = 0;
 
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                sum += getPerimeter(grid, i, j);
+            }
+        }
+
+        return sum;
+    }
+
+    private int getPerimeter(int[][] grid, int r, int c) {
+        if (!isValidLand(grid, r, c)) {
+            return 0;
+        }
+
+        int perimeter = 4;
+
+        if (isValidLand(grid, r - 1, c)) {
+            perimeter -= 1;
+        }
+        if (isValidLand(grid, r + 1, c)) {
+            perimeter -= 1;
+        }
+        if (isValidLand(grid, r, c - 1)) {
+            perimeter -= 1;
+        }
+        if (isValidLand(grid, r, c + 1)) {
+            perimeter -= 1;
+        }
+
+        return perimeter;
+    }
+
+    private boolean isValidLand(int[][] grid, int r, int c) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (r >= 0 && r < rows && c >= 0 && c < cols) {
+            return grid[r][c] == 1;
+        }
+
+        return false;
     }
 }
