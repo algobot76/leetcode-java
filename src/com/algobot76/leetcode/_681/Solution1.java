c70d4cb1d8249b096409200007307166c30f22ef
Kaitian Xie <xkaitian@gmail.com>
2019-04-19 00:42:28 -0700

    681: Brute Force

--- src/com/algobot76/leetcode/_681/Solution1.java
+++ src/com/algobot76/leetcode/_681/Solution1.java
@@ -1,7 +1,32 @@
 package com.algobot76.leetcode._681;
 
+/**
+ * Brute Force
+ */
 public class Solution1 {
     public String nextClosestTime(String time) {
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int min = Integer.parseInt(time.substring(3, 5));
 
+        while (true) {
+            if (++min == 60) {
+                min = 0;
+                hour++;
+                hour %= 24;
+            }
+            String curr = String.format("%02d:%02d", hour, min);
+            if (isValid(time, curr)) {
+                return curr;
+            }
+        }
+    }
+
+    private boolean isValid(String time, String curr) {
+        for (int i = 0; i < curr.length(); i++) {
+            if (time.indexOf(curr.charAt(i)) < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
