28c3cbc9a4f58023e526a428b407fc5bf412f1b1
Kaitian Xie <xkaitian@gmail.com>
2019-04-24 14:40:42 -0700

    41: finish problem

--- src/com/algobot76/leetcode/_41/Solution1.java
+++ src/com/algobot76/leetcode/_41/Solution1.java
@@ -2,6 +2,23 @@ package com.algobot76.leetcode._41;
 
 public class Solution1 {
     public int firstMissingPositive(int[] nums) {
-        
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            nums[i]--;
+        }
+        for (int i = 0; i < len; i++) {
+            while (nums[i] != i && nums[i] >= 0 && nums[i] < len && nums[i] != nums[nums[i]]) {
+                int temp = nums[i];
+                nums[i] = nums[nums[i]];
+                nums[temp] = temp;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
     }
 }
