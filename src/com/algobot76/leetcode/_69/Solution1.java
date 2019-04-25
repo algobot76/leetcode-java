cfc78a3f953e77aa760d7f3fcdd827c0af38da85
Kaitian Xie <xkaitian@gmail.com>
2019-04-25 15:56:37 -0700

    69: Brute Force (Multiplication)

--- src/com/algobot76/leetcode/_69/Solution1.java
+++ src/com/algobot76/leetcode/_69/Solution1.java
@@ -1,7 +1,18 @@
 package com.algobot76.leetcode._69;
 
+/**
+ * Brute Force (Multiplication)
+ */
 public class Solution1 {
     public int mySqrt(int x) {
-
+        if (x <= 1) {
+            return x;
+        }
+        for (long s = 1; s <= x; s++) {
+            if (s * s > x) {
+                return (int) s - 1;
+            }
+        }
+        return -1;
     }
 }
