cf98e283054298e46335d1c5fa34b7536e63dabb
Kaitian Xie <xkaitian@gmail.com>
2019-04-26 12:11:18 -0700

    162: Binary Search

--- src/com/algobot76/leetcode/_162/Solution1.java
+++ src/com/algobot76/leetcode/_162/Solution1.java
@@ -1,7 +1,20 @@
 package com.algobot76.leetcode._162;
 
+/**
+ * Binary Search
+ */
 public class Solution1 {
     public int findPeakElement(int[] nums) {
-
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (left == mid) {
+                return nums[left] < nums[right] ? right : left;
+            }
+            left = nums[mid] < nums[mid + 1] ? mid : left;
+            right = nums[mid] > nums[mid + 1] ? mid : right;
+        }
+        return 0;
     }
 }
