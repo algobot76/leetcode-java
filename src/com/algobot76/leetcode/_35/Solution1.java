03c484e872575cea04db76327ecb8930b75b8626
Kaitian Xie <xkaitian@gmail.com>
2019-04-25 19:13:43 -0700

    35: Binary Search

--- src/com/algobot76/leetcode/_35/Solution1.java
+++ src/com/algobot76/leetcode/_35/Solution1.java
@@ -1,7 +1,25 @@
 package com.algobot76.leetcode._35;
 
+/**
+ * Binary Search
+ */
 public class Solution1 {
     public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
