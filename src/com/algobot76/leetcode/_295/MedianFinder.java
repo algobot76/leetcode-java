2750a4871251c796b081ade40d522065569da656
Kaitian Xie <xkaitian@gmail.com>
2019-05-05 11:19:00 -0700

    295: Two Heaps

--- src/com/algobot76/leetcode/_295/MedianFinder.java
+++ src/com/algobot76/leetcode/_295/MedianFinder.java
@@ -1,19 +1,41 @@
 package com.algobot76.leetcode._295;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Two Heaps
+ */
 class MedianFinder {
+    private Queue<Integer> maxHeap;
+    private Queue<Integer> minHeap;
 
     /**
      * initialize your data structure here.
      */
     public MedianFinder() {
-
+        maxHeap = new PriorityQueue<>();
+        minHeap = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
-
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
+        }
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        } else if (maxHeap.size() - minHeap.size() == 2) {
+            minHeap.offer(maxHeap.poll());
+        }
     }
 
     public double findMedian() {
-
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        } else {
+            return (maxHeap.peek() + minHeap.peek()) / 2;
+        }
     }
 }
