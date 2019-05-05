9a3cef26790226b203c225e09cb495ffca350bfe
Kaitian Xie <xkaitian@gmail.com>
2019-05-05 10:46:43 -0700

    295: begin problem

---
+++ src/com/algobot76/leetcode/_295/MedianFinder.java
@@ -0,0 +1,19 @@
+package com.algobot76.leetcode._295;
+
+class MedianFinder {
+
+    /**
+     * initialize your data structure here.
+     */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
