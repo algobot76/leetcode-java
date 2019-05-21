9061e419aaa928eead5448da3863bf1fcbffa07e
Kaitian Xie <xkaitian@gmail.com>
2019-05-21 11:02:56 -0700

    36: Finish problem

--- src/com/algobot76/leetcode/_36/Solution1.java
+++ src/com/algobot76/leetcode/_36/Solution1.java
@@ -1,7 +1,52 @@
 package com.algobot76.leetcode._36;
 
+import java.util.Arrays;
+
 public class Solution1 {
     public boolean isValidSudoku(char[][] board) {
+        boolean[] visited = new boolean[9];
 
+        for (int i = 0; i < 9; i++) {
+            Arrays.fill(visited, false);
+            for (int j = 0; j < 9; j++) {
+                if (!check(visited, board[i][j])) {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            Arrays.fill(visited, false);
+            for (int j = 0; j < 9; j++) {
+                if (!check(visited, board[j][i])) {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                Arrays.fill(visited, false);
+                for (int k = 0; k < 9; k++) {
+                    if (!check(visited, board[i + k / 3][j + k % 3])) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean check(boolean[] visited, char digit) {
+        if (digit == '.') {
+            return true;
+        }
+        int num = digit - '0';
+        if (num < 0 || num > 9 || visited[num - 1]) {
+            return false;
+        }
+        visited[num - 1] = true;
+        return true;
     }
 }
