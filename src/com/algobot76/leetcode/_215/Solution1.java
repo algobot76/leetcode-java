3b358b3f729d7429d68357a77705dca7e4b89baf
Kaitian Xie <xkaitian@gmail.com>
2019-05-21 17:08:31 -0700

    215: small fixes

--- src/com/algobot76/leetcode/_215/Solution1.java
+++ src/com/algobot76/leetcode/_215/Solution1.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
  * Sort
  */
 public class Solution1 {
-        public int findKthLargest(int[] nums, int k) {
-            Arrays.sort(nums);
-            return nums[nums.length - k];
-        }
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
 }
