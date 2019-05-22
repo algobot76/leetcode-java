7bc190e01925e66a30ffd82aead24f8a1df572a2
Kaitian Xie <xkaitian@gmail.com>
2019-05-21 18:10:26 -0700

    125: Two Pointers

--- src/com/algobot76/leetcode/_125/Solution1.java
+++ src/com/algobot76/leetcode/_125/Solution1.java
@@ -1,7 +1,32 @@
 package com.algobot76.leetcode._125;
 
+
+/**
+ * Two Pointers
+ */
 public class Solution1 {
     public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
 
+        while (left < right) {
+            while (left < s.length() && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            if (left == s.length()) {
+                return true;
+            }
+            while (right >= 0 && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                break;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return right <= left;
     }
 }
