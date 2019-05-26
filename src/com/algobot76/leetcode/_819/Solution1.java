86e4849d9cb06bbf911fde326a6b3cf1425d9eca
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 01:43:49 -0700

    819: finish problem

--- src/com/algobot76/leetcode/_819/Solution1.java
+++ src/com/algobot76/leetcode/_819/Solution1.java
@@ -1,7 +1,27 @@
 package com.algobot76.leetcode._819;
 
+import java.util.*;
+
 public class Solution1 {
     public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> bannedSet = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = paragraph.replaceAll("[^a-zA-Z]", " ").toLowerCase().split("\\s+");
+        for (String word : words) {
+            if (!bannedSet.contains(word)) {
+                count.put(word, count.getOrDefault(word, 0) + 1);
+            }
+        }
+        int max = 0;
+        String ans = "";
+        for (String word : count.keySet()) {
+            int freq = count.get(word);
+            if (freq > max) {
+                ans = word;
+                max = freq;
+            }
+        }
 
+        return ans;
     }
 }
