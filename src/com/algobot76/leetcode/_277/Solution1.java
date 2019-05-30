595dc81bda222ca2e624e8a7ea2444807ee54256
Kaitian Xie <xkaitian@gmail.com>
2019-05-30 12:00:27 -0700

    277: finish problem

--- src/com/algobot76/leetcode/_277/Solution1.java
+++ src/com/algobot76/leetcode/_277/Solution1.java
@@ -1,7 +1,25 @@
 package com.algobot76.leetcode._277;
 
-public class Solution1 {
-    public int findCelebrity(int n) {
+import com.algobot76.leetcode.util.Relation;
 
+public class Solution1 extends Relation {
+    public int findCelebrity(int n) {
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(ans, i)) {
+                ans = i;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (ans != i && knows(ans, i)) {
+                return -1;
+            }
+            if (ans != i && !knows(i, ans)) {
+                return -1;
+            }
+        }
+
+        return ans;
     }
 }
---
+++ src/com/algobot76/leetcode/util/Relation.java
@@ -0,0 +1,7 @@
+package com.algobot76.leetcode.util;
+
+public class Relation {
+    public boolean knows(int a, int b) {
+        return true; // place holder
+    }
+}
