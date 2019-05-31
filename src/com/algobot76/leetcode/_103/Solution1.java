f0733bce6bb98f9c7fba3421894fcf21b12ca993
Kaitian Xie <xkaitian@gmail.com>
2019-05-30 20:51:59 -0700

    103: begin problem

---
+++ src/com/algobot76/leetcode/_103/Solution1.java
@@ -0,0 +1,11 @@
+package com.algobot76.leetcode._103;
+
+import com.algobot76.leetcode.util.TreeNode;
+
+import java.util.List;
+
+public class Solution1 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+}
