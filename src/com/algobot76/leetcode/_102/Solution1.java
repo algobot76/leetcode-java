014076a128b575a7b78ed2ceb6c4fa27f1d5e5e7
Kaitian Xie <xkaitian@gmail.com>
2019-06-04 14:59:48 -0700

    102: BFS

--- src/com/algobot76/leetcode/_102/Solution1.java
+++ src/com/algobot76/leetcode/_102/Solution1.java
@@ -2,10 +2,39 @@ package com.algobot76.leetcode._102;
 
 import com.algobot76.leetcode.util.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
+/**
+ * BFS
+ */
 public class Solution1 {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
 
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode n = q.poll();
+                level.add(n.val);
+                if (n.left != null) {
+                    q.offer(n.left);
+                }
+                if (n.right != null) {
+                    q.offer(n.right);
+                }
+            }
+            ans.add(level);
+        }
+
+        return ans;
     }
 }
