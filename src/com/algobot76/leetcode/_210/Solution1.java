bac727186a4492e2dab09b3f9cd7487f098673d8
Kaitian Xie <xkaitian@gmail.com>
2019-06-14 16:54:29 -0700

    210: Topological Order;

--- src/com/algobot76/leetcode/_210/Solution1.java
+++ src/com/algobot76/leetcode/_210/Solution1.java
@@ -1,7 +1,51 @@
 package com.algobot76.leetcode._210;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Topological Order
+ */
 public class Solution1 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] ans = new int[numCourses];
+        List[] graph = new ArrayList[numCourses];
+        int[] inDegree = new int[numCourses];
 
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            inDegree[prerequisites[i][0]]++;
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+        int count = 0;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            ans[count] = curr;
+            count++;
+            List<Integer> courses = graph[curr];
+            for (int i = 0; i < courses.size(); i++) {
+                int next = courses.get(i);
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        if (count == numCourses) {
+            return ans;
+        } else {
+            return new int[0];
+        }
     }
 }
