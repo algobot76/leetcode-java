361868ec1716a7e900dec66d8faa5ccb6221f3d0
Kaitian Xie <xkaitian@gmail.com>
2019-06-14 16:28:25 -0700

    207: small fixes

--- src/com/algobot76/leetcode/_207/Solution1.java
+++ src/com/algobot76/leetcode/_207/Solution1.java
@@ -10,32 +10,32 @@ import java.util.Queue;
  */
 public class Solution1 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List[] edges = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
+        List[] graph = new ArrayList[numCourses];
+        int[] inDegree = new int[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
-            edges[i] = new ArrayList<Integer>();
+            graph[i] = new ArrayList<Integer>();
         }
         for (int i = 0; i < prerequisites.length; i++) {
-            degree[prerequisites[i][0]]++;
-            edges[prerequisites[i][1]].add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
         }
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < degree.length; i++) {
-            if (degree[i] == 0) {
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
                 q.add(i);
             }
         }
         int count = 0;
         while (!q.isEmpty()) {
-            int course = q.poll();
+            int curr = q.poll();
             count++;
-            int n = edges[course].size();
-            for (int i = 0; i < n; i++) {
-                int pointer = (int) edges[count].get(i);
-                degree[pointer]--;
-                if (degree[pointer] == 0) {
-                    q.add(pointer);
+            List<Integer> courses = graph[curr];
+            for (int i = 0; i < courses.size(); i++) {
+                int next = courses.get(i);
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    q.offer(next);
                 }
             }
         }
