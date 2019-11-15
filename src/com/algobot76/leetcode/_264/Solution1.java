50afcc5ed08d8ea63b42dfa6da0de51e764e4c5d
Kaitian Xie <xkaitian@gmail.com>
2019-11-14 22:28:34 -0800

    264: Solve problem using priority queue

--- src/com/algobot76/leetcode/_264/Solution1.java
+++ src/com/algobot76/leetcode/_264/Solution1.java
@@ -1,16 +1,30 @@
 package com.algobot76.leetcode._264;
 
-
-import java.util.*;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 public class Solution1 {
     public int nthUglyNumber(int n) {
-        Queue<Integer> pq = new PriorityQueue<>(Arrays.asList(1));
-        Set<Integer> visited = new HashSet<>();
+        Queue<Long> pq = new PriorityQueue<>();
+        pq.offer((long) 1);
+        Set<Long> visited = new HashSet<>();
+        visited.add((long) 1);
 
-        int res = 0;
+        int[] factors = new int[]{2, 3, 5};
 
-
-
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            res = pq.poll();
+            for (int factor : factors) {
+                long num = res * factor;
+                if (!visited.contains(num)) {
+                    visited.add(num);
+                    pq.offer(num);
+                }
+            }
+        }
+        return (int) res;
     }
 }
